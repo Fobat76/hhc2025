2a80c814daf618c763155d5d7a86009ccc619a31
Devin Chen <Devinc23456@gmail.com>
2025-05-03 16:02:07 -0400

    stuff

--- scripts/Items/Item.gd
+++ scripts/Items/Item.gd
@@ -3,9 +3,21 @@ class_name Item
 
 @export var item_type: Items.ItemType
 @onready var sprite = $Sprite2D
+@onready var label = $Label
+
+func _ready():
+	label.visible = false  # Hide by default
 
 func pickup(player):
 	if player.held_item_type == -1:
 		player.held_item_type = item_type
 		print("Item is picked up")
 		queue_free()  # or hide()
+
+func _on_area_entered(area):
+	if area.is_in_group("player"):
+		label.visible = true
+
+func _on_area_exited(area):
+	if area.is_in_group("player"):
+		label.visible = false
