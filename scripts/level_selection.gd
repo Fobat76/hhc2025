9cebe5ff4c62eacfc4eb0f1412a25a07ea9f0576
Darren Lin <darrenlin180@gmail.com>
2025-05-03 00:15:13 -0400

    fixed buttons not working

--- scripts/level_selection.gd
+++ scripts/level_selection.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 
 func _on_back_to_menu_pressed() -> void:
-	get_tree().change_scene("res://scenes/menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -4,10 +4,14 @@ func _ready():
 	$PlayButton.grab_focus()
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene("res://scenes/Level_Selection.tscn")
+	get_tree().change_scene_to_file("res://scenes/Level_Selection.tscn")
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/options.tscn")
+
+func _on_extra_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/extra_credits.tscn")
+
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-
-func _on_settings_pressed() -> void:
-	get_tree().change_scene("res://scenes/options.tscn")
