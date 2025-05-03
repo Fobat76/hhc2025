4ffbb1e8f29d5a8ad7179cb06a66756441b5d2e8
Darren Lin <darrenlin180@gmail.com>
2025-05-03 18:33:10 -0400

    added Pause menu

--- scripts/Main Menu/menu.gd
+++ scripts/Main Menu/menu.gd
@@ -8,11 +8,15 @@ func _on_play_button_pressed() -> void:
 	$CanvasLayer/LevelSelection.set_visibility_layer_bit(1, true)
 
 func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Main Menu/options.tscn")
+	var settingsScene = load("res://scenes/Main Menu/options.tscn").instantiate()
+	self.add_child(settingsScene)
+	$CanvasLayer/LevelSelection.set_visibility_layer_bit(1, false)
+
 
 func _on_extra_credits_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Main Menu/extra_credits.tscn")
-
+	var extraScene = load("res://scenes/Main Menu/extra_credits.tscn").instantiate()
+	self.add_child(extraScene)
+	$CanvasLayer/LevelSelection.set_visibility_layer_bit(1, false)
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
---
+++ scripts/Player/pause_menu.gd
@@ -0,0 +1,32 @@
+extends Control
+
+func _ready():
+	$AnimationPlayer.play("RESET")
+
+func resume():
+	get_tree().paused = false
+	$TextureRect.set_visibility_layer_bit(1, false)
+
+func pause():
+	get_tree().paused = true
+	$TextureRect.set_visibility_layer_bit(1, true)
+
+func testEsc():
+	if Input.is_action_just_pressed("escape") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("escape") and get_tree().paused == true:
+		resume()
+ 
+func _on_resume_pressed() -> void:
+	resume()
+
+func _on_restart_pressed() -> void:
+	resume()
+	get_tree().reload_current_scene()
+
+func _on_quit_pressed() -> void:
+	resume()
+	get_tree().change_scene_to_file("res://scenes/Main Menu/menu.tscn")
+
+func _process(delta):
+	testEsc()
