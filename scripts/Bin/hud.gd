ed2fecb2d9e0d72b5196bd2bcaeac6355df09eb7
Darren Lin <darrenlin180@gmail.com>
2025-05-04 01:57:37 -0400

    completed how to play scene

--- scripts/Bin/hud.gd
+++ scripts/Bin/hud.gd
@@ -9,12 +9,12 @@ var track = 0
 func _ready():
 	global.connect("on_wrong", self._on_wrong)
 	global.connect("on_insert", self._on_insert)
-	
+
 func update_spawned_items(string : String):
 	spawned_items.text = string
 
 func _on_wrong():
-	if incorrect.text.length() - 7 == 2:
+	if incorrect.text.length() == 2:
 		get_tree().change_scene_to_file("res://scenes/lose.tscn") 
 	else:
 		incorrect.text += "X"
