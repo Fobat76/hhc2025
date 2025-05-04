d200ca0ef2bd5391303f14de5936ed986a0aebc5
Darren Lin <darrenlin180@gmail.com>
2025-05-04 00:29:35 -0400

    added default menus

--- scripts/Main Menu/level_selection.gd
+++ scripts/Main Menu/level_selection.gd
@@ -18,3 +18,50 @@ func _on_button_4_pressed() -> void:
 
 func _on_button_5_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Levels/level5.tscn")
+
+@onready var level1Button = $HBoxContainer/GoLevel1
+@onready var img1 = $Level1Blurred
+
+@onready var level2Button = $HBoxContainer/GoLevel2
+@onready var img2 = $Level2Blurred
+
+@onready var level3Button = $HBoxContainer/GoLevel3
+@onready var img3 = $Level3Blurred
+
+@onready var level4Button = $HBoxContainer/GoLevel4
+@onready var img4 = $Level4Blurred
+
+
+func _ready() -> void:
+	level1Button.connect("mouse_entered", Callable(self, "_on_button1_hovered"))
+	level1Button.connect("mouse_exited", Callable(self, "_on_button1_unhovered"))
+	level2Button.connect("mouse_entered", Callable(self, "_on_button2_hovered"))
+	level2Button.connect("mouse_exited", Callable(self, "_on_button2_unhovered"))
+	level3Button.connect("mouse_entered", Callable(self, "_on_button3_hovered"))
+	level3Button.connect("mouse_exited", Callable(self, "_on_button3_unhovered"))
+	level4Button.connect("mouse_entered", Callable(self, "_on_button4_hovered"))
+	level4Button.connect("mouse_exited", Callable(self, "_on_button4_unhovered"))
+
+func _on_button1_hovered():
+	img1.visible = true
+
+func _on_button1_unhovered():
+	img1.visible = false
+
+func _on_button2_hovered():
+	img2.visible = true
+
+func _on_button2_unhovered():
+	img2.visible = false
+
+func _on_button3_hovered():
+	img3.visible = true
+
+func _on_button3_unhovered():
+	img3.visible = false
+
+func _on_button4_hovered():
+	img4.visible = true
+
+func _on_button4_unhovered():
+	img4.visible = false
