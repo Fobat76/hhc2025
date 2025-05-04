2f36a89d661416104d3c8c6a083ab9e5c270c61f
Darren Lin <darrenlin180@gmail.com>
2025-05-03 22:25:40 -0400

    implemented HUD but there's bugs

---
+++ scripts/Levels/level_3.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+const numberOfItems = 15
+
+func _ready():
+	for i in numberOfItems:
+		get_node("ItemSpawn").spawn()
+		await get_tree().create_timer(3).timeout
---
+++ scripts/Levels/level_5.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+const numberOfItems = 25
+
+func _ready():
+	for i in numberOfItems:
+		get_node("ItemSpawn").spawn()
+		await get_tree().create_timer(3).timeout
