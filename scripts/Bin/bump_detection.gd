86c7638080030b4465ca38e0ba67e709f7cbbc4e
Devin Chen <Devinc23456@gmail.com>
2025-05-04 05:22:55 -0400

    decor

--- scripts/Bin/bump_detection.gd
+++ scripts/Bin/bump_detection.gd
@@ -3,17 +3,17 @@ extends Area2D
 
 func _on_body_entered(body):
 	if not body.is_in_group("player"):
-		#print("nothing")
 		return  # Don't do anything if it's not the player
 
 	if body.held_item_type != -1:
 		if body.held_item_type == bin_type:
-			#print("✅ Correct")
 			global.emit_signal("on_insert")
+			correct_feedback()
+			get_parent().get_node("CorrectSound").play()
 		else:
-			#print("❌ Wrong")
 			global.emit_signal("on_wrong")
 			shake()
+			get_parent().get_node("WrongSound").play()
 		body.held_item_type = -1
 
 func shake():
@@ -24,3 +24,9 @@ func shake():
 	tween.tween_property(parent, "position", original_pos - Vector2(5, 0), 0.05)
 	tween.tween_property(parent, "position", original_pos + Vector2(3, 0), 0.05)
 	tween.tween_property(parent, "position", original_pos, 0.05)
+
+func correct_feedback():
+	var parent = get_parent()
+	var tween = parent.create_tween()
+	tween.tween_property(parent, "scale", Vector2(1.1, 1.1), 0.05).set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(parent, "scale", Vector2(1, 1), 0.1).set_trans(Tween.TRANS_ELASTIC)
--- scripts/Conveyor/conveyor.gd
+++ scripts/Conveyor/conveyor.gd
@@ -1,64 +1,9 @@
 extends Area2D
 class_name Conveyor
 
-@onready var item_holder = $ItemHolder
-@onready var detector = $Detector
-enum Direction {Left, Right, Up, Down}
-@export var to_direction: Direction = Direction.Left
-@export var from_direction: Direction = Direction.Right
-@onready var sprite = $Sprite2D
+@onready var sprite = $AnimatedSprite2D
 
-func set_direction():
-	match to_direction:
-		Direction.Left:
-			detector.position = Vector2.LEFT * 32
-			match from_direction:
-				Direction.Right:
-					sprite.frame = 1
-				Direction.Up:
-					sprite.frame = 9
-				Direction.Down:
-					sprite.frame = 2
-		Direction.Right:
-			detector.position = Vector2.RIGHT * 32
-			match from_direction:
-				Direction.Left:
-					sprite.frame = 11
-				Direction.Up:
-					sprite.frame = 10
-				Direction.Down:
-					sprite.frame = 3
-		Direction.Up:
-			detector.position = Vector2.UP * 32
-			match from_direction:
-				Direction.Left:
-					sprite.frame = 12
-				Direction.Right:
-					sprite.frame = 7
-				Direction.Down:
-					sprite.frame = 8
-		Direction.Down:
-			detector.position = Vector2.DOWN * 32
-			match from_direction:
-				Direction.Left:
-					sprite.frame = 4
-				Direction.Up:
-					sprite.frame = 5
-				Direction.Right:
-					sprite.frame = 0
 
 func _ready():
-	set_direction()
+	sprite.play()
 	
-func can_receive_item():
-	return item_holder.get_child_count() == 0
-	
-func receive_item(item: Node2D):
-	item_holder.receive_item(item)
-	
-func _on_detector_belt_detected(area: Area2D):
-	var item = item_holder.offload_item()
-	area.receive_item(item)
-	
-func _on_item_holder_item_held():
-	detector.detect()
