4985921c79bcfce0eab18c5d2e45bd71bad08c57
Darren Lin <darrenlin180@gmail.com>
2025-05-04 01:20:28 -0400

    added level 5 and transitioning to losing and winning scenes'

--- scripts/Levels/level_5.gd
+++ scripts/Levels/level_5.gd
@@ -1,8 +1,26 @@
 extends Node2D
 
 const numberOfItems = 25
+var curNum = 0
+var track = 1
 
-func _ready():
-	for i in numberOfItems:
+func _ready():	
+	global.connect("on_wrong", self._on_wrong)
+	global.connect("on_insert", self._on_insert)
+	while curNum != numberOfItems:
+		while get_tree().paused == true:
+			await get_tree().create_timer(3).timeout
 		get_node("ItemSpawn").spawn()
+		curNum += 1
+		$CanvasLayer/Hud.update_spawned_items("Spawned Items: " + str(curNum) + "/" + str(numberOfItems))
 		await get_tree().create_timer(3).timeout
+
+func _on_wrong():
+	if track == numberOfItems:
+		get_tree().change_scene_to_file("res://scenes/win.tscn")
+	track += 1
+
+func _on_insert():
+	if track == numberOfItems:
+		get_tree().change_scene_to_file("res://scenes/win.tscn")
+	track += 1
